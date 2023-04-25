86430e259a82e6b3b42e53fe127a8068d8f062d8
AlanHustler <110681275+AlanHustler@users.noreply.github.com>
2023-04-25 10:50:40 +0600

    files

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def connect():
         crsr.execute('SELECT version()')
         db_version = crsr.fetchone()
         print(db_version)
+
+        crsr.execute("DROP TABLE IF EXISTS employee")
+
         create_script = """CREATE TABLE IF NOT EXISTS employee (
             id  int PRIMARY KEY,
             name    varchar(40) NOT NULL,
@@ -24,8 +27,12 @@ def connect():
         crsr.execute(create_script)
 
         insert_script = "INSERT INTO employee(id,name,salary, dept_id) VALUES (%s,%s,%s,%s)"
-        insert_value = (2,"Dany", 16000, 'G1')
-        crsr.execute(insert_script, insert_value)
+        insert_value = [(1,"James",16000,'F1'),(2,"JoJo", 26000, 'D1'),(3, "Alan", 63000, "F1")]
+        for record in insert_value:
+            crsr.execute(insert_script, record)
+
+        crsr.execute('SELECT * FROM employee')
+        print(crsr.fetchall())
 
         connection.commit()
         crsr.close()
